a30da59337db31b6e0b442ee0ce6daf7d07ad6e3
Tristan White <user@example.com>
2025-10-10 15:49:33 -0400

    added Layout class

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # navfitx
 NextGen NAVFIT
+
+# Relevant Documents
+
+[NAVFIT98A v30 User Guide Manual](https://www.mynavyhr.navy.mil/Portals/55/Career/PerformanceEvaluation/NAVFIT98A%20Version%2030%20user%20guide.pdf?ver=rBFhxjABpJhUybBeMo6AMA%3d%3d)
+
+[BUPERSINT 1610.10H](https://www.mynavyhr.navy.mil/Portals/55/Reference/Instructions/BUPERS/BUPERSINST%201610.10.pdf?ver=DZVcHnNH8gLkDjKjDFyaKA%3d%3d)
+
+
--- navfitx/fitrep.py
+++ navfitx/fitrep.py
@@ -4,6 +4,7 @@ from constants import MARGIN_SIDE, MARGIN_TOP, MARGIN_BOTTOM
 from reportlab.lib.pagesizes import LETTER
 from reportlab.lib.units import inch
 from reportlab.pdfgen.canvas import Canvas
+from reportlab.pdfbase.acroform import AcroForm
 
 @dataclass
 class Box:
@@ -59,13 +60,11 @@ def make_box_below(box: Box, width: float, height: float) -> Box:
     """
     return Box(bl=(box.bl[0], box.bl[1] - height), tr=(box.bl[0] + width, box.br[1]))
 
-def draw_label_0(canvas: Canvas, box: Box, text: str, size: int = 8):
-    canvas.setFont("Times-Roman", size)
-    canvas.drawString(box.tl[0] + 3, box.tl[1] - box.height / 3, text, wordSpace=1.0)
+
 
 # Row 0
 box_0 = Box(
-    bl=(MARGIN_SIDE, LETTER[1] - MARGIN_TOP - inch / 3),
+    bl=(MARGIN_SIDE, LETTER[1] - MARGIN_TOP - 24),
     tr=(LETTER[0] / 2, LETTER[1] - MARGIN_TOP),
 )
 box_1 = make_box_to_right(box_0, 66)
@@ -73,24 +72,24 @@ box_2 = make_box_to_right(box_1, 97)
 box_3 = make_box_to_right(box_2, LETTER[0] - MARGIN_SIDE - box_2.br[0])
 
 # Row 1
-box_4 = make_box_below(box_0, 152, inch / 3)
+box_4 = make_box_below(box_0, 152, box_0.height)
 box_5 = make_box_to_right(box_4, 50)
 box_6 = make_box_to_right(box_5, 194)
 box_7 = make_box_to_right(box_6, 76)
 box_8 = make_box_to_right(box_7, LETTER[0] - MARGIN_SIDE - box_7.br[0])
 
 # Row 2
-box_9 = make_box_below(box_4, 370 - MARGIN_SIDE - 2, inch / 3)
+box_9 = make_box_below(box_4, 370 - MARGIN_SIDE - 2, box_0.height)
 box_10 = make_box_to_right(box_9, LETTER[0] - 2 * MARGIN_SIDE - box_9.width)
 
 # Row 3
-box_11 = make_box_below(box_9, 112 - MARGIN_SIDE, inch / 3)
+box_11 = make_box_below(box_9, 112 - MARGIN_SIDE, box_0.height)
 box_12 = make_box_to_right(box_11, box_9.width - box_11.width)
 box_13 = make_box_to_right(box_12, 98)
 box_14 = make_box_to_right(box_13, box_10.width - box_13.width)
 
 # Row 4
-box_15 = make_box_below(box_11, box_4.width - 1, box_11.height)
+box_15 = make_box_below(box_11, box_4.width - 1, box_0.height)
 box_16 = make_box_to_right(box_15, 48)
 box_17 = make_box_to_right(box_16, box_16.width + 2)
 box_18 = make_box_to_right(box_17, 412 - box_17.br[0])
@@ -110,7 +109,7 @@ box_25 = make_box_to_right(box_24, 140)
 box_26 = Box(bl=box_25.br, tr=box_22.br)
 
 # Row 8
-box_27 = make_box_below(box_23, box_21.width, inch / 4)
+box_27 = make_box_below(box_23, box_21.width, inch / 4 + 1)
 
 # Row 9
 box_28 = make_box_below(box_27, inch, 30)
@@ -181,40 +180,112 @@ boxes = [
     box_58, box_59, box_60, box_61, box_62, box_63,
 ]
 
+def draw_b23_text(c: Canvas):
+    text = """
+    For Mid-term Counseling Use. (When completing FITREP
+    enter 30 and 31 from counseling worksheet sign 32.)
+    """
+    t = c.beginText(box_23.tl[0] + 2, box_23.tl[1] - 2)
+    t.setLeading(8)
+    c.setFont("Times-Roman", 7)
+    for line in text.splitlines():
+        t.textLine(line.strip())
+    c.drawText(t)
 
+def draw_b27_text(c: Canvas):
+    text = """
+    PERFORMANCE TRAITS:  1.0 - Below standards/not progressing or UNSAT in any one standard;  2.0 - Does not yet meet all 3.0 standards;  3.0 - Meets all 3.0
+    standards;  4.0 - Exceeds most 3.0 standards;  5.0 - Meets overall criteria and most of the specific standards for 5.0.  Standards are not all inclusive.
+    """
+    t = c.beginText(box_27.tl[0] + 2, box_27.tl[1] + 2)
+    t.setLeading(7)
+    t.setFont("Times-Roman", 8.2)
+    for line in text.splitlines():
+        t.textLine(line.strip())
+    c.drawText(t)
 
+class Layout():
+    def __init__(self, canvas: Canvas):
+        self.canvas = canvas
+        self.canvas.setLineWidth(0.5)
+
+    def draw_label_standard(self, box: Box, text: str, size: int = 8):
+        self.canvas.setFont("Times-Roman", size)
+        self.canvas.drawString(box.tl[0] + 3, box.tl[1] - 8, text, wordSpace=1.0)
+
+    def draw_checkbox_br(self, box: Box):
+        self.canvas.rect(box.br[0] - 18, box.br[1] + 2, 14, 12)
+
+    def draw_pt_row_header(self, box: Box, blk_num: int, text: str):
+        header, desc = text.split(":")
+        string = f"{blk_num}.\n"
+        txt = 
+
+    def draw(self):
+        # use the instance canvas
+        c = self.canvas
+
+        for i, box in enumerate(boxes):
+            box.draw_left_border(c)
+            box.draw_right_border(c)
+            box.draw_top_border(c)
+            # box.draw_debug(c, str(i))
+        
+        # draw bottom border on box 58-63
+        for box in [box_58, box_59, box_60, box_61, box_62, box_63]:
+            box.draw_bottom_border(c)
+        
+
+        # use the instance method to draw labels
+        self.draw_label_standard(box_0, "1.  Name (Last, First  MI  Suffix)")
+        self.draw_label_standard(box_1, "2.  Grade/Rate")
+        self.draw_label_standard(box_2, "3.  Desig")
+        self.draw_label_standard(box_3, "4.  SSN")
+        self.draw_label_standard(box_4, "5.")
+        self.draw_label_standard(box_5, "6.  UIC")
+        self.draw_label_standard(box_6, "7.  Ship/Station")
+        self.draw_label_standard(box_7, "8.  Promotion Status")
+        self.draw_label_standard(box_8, "9.  Date Reported")
+
+        self.draw_label_standard(box_9, "Occasion for Report", size=7)
+
+        self.draw_label_standard(box_10, "Period of Report", size=7)
+
+        self.draw_label_standard(box_11, "16.  Not Observed", size=7)
+        self.draw_checkbox_br(box_11)
+
+        self.draw_label_standard(box_12, "Type of Report")
+        self.draw_label_standard(box_13, "20.  Physical Readiness")
+        self.draw_label_standard(box_14, "21.  Billet Subcategory")
+        self.draw_label_standard(box_15, "22.  Reporting Senior (Last, FI  MI)")
+        self.draw_label_standard(box_16, "23.  Grade")
+        self.draw_label_standard(box_17, "24.  Desig")
+        self.draw_label_standard(box_18, "25.  Title")
+        self.draw_label_standard(box_19, "26.  UIC")
+        self.draw_label_standard(box_20, "27.  SSN")
+        self.draw_label_standard(box_21, "28.  Command employment and command achievements.", size=7)
+
+        self.draw_label_standard(box_22, "29.  Primary/Collateral/Watchstanding duties.  (Enter primary duty abbreviation in box.)", size=7)
+        self.canvas.rect(box_22.tl[0] + 3, box_22.tl[1] - 19, 118, 10)
+
+        self.draw_label_standard(box_24, "30.  Date Counseled")
+        self.draw_label_standard(box_25, "31.  Counselor")
+        self.draw_label_standard(box_26, "32.  Signature of Individual Counseled")
+
+        draw_b23_text(c)
+        draw_b27_text(c)
+
+        # draw a check box in the bottom right corner of box 34-63
+        for box in [box_34, box_35, box_36, box_37, box_38, box_39,
+                    box_40, box_41, box_42, box_43, box_44, box_45,
+                    box_46, box_47, box_48, box_49, box_50, box_51,
+                    box_52, box_53, box_54, box_55, box_56, box_57,
+                    box_58, box_59, box_60, box_61, box_62, box_63]:
+            self.draw_checkbox_br(box)
 
 if __name__ == "__main__":
     c = Canvas("layout.pdf", pagesize=LETTER)
-    c.setLineWidth(0.5)
-    
-    for i, box in enumerate(boxes):
-        box.draw_left_border(c)
-        box.draw_right_border(c)
-        box.draw_top_border(c)
-        # box.draw_debug(c, str(i))
-    
-    # draw bottom border on box 58-63
-    for box in [box_58, box_59, box_60, box_61, box_62, box_63]:
-        box.draw_bottom_border(c)
-    
-
-    draw_label_0(c, box_0, "1.  Name (Last, First  MI  Suffix)")
-    draw_label_0(c, box_1, "2.  Grade/Rate")
-    draw_label_0(c, box_2, "3.  Desig")
-    draw_label_0(c, box_3, "4.  SSN")
-    draw_label_0(c, box_4, "5.")
-    draw_label_0(c, box_5, "6.  UIC")
-    draw_label_0(c, box_6, "7.  Ship/Station")
-    draw_label_0(c, box_7, "8.  Promotion Status")
-    draw_label_0(c, box_8, "9.  Date Reported")
-    draw_label_0(c, box_9, "Occasion for Report", size=7)
-
-    draw_label_0(c, box_13, "20.  Physical Readiness")
-    draw_label_0(c, box_14, "21.  Billet Subcategory")
-
-    draw_label_0(c, box_22, "")
-
-
-    c.showPage()
-    c.save()
+    layout = Layout(canvas=c)
+    layout.draw()
+    layout.canvas.showPage()
+    layout.canvas.save()
